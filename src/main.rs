6931d99b41c594e1021ac169322493e1fd89ad13
gerard-fabrego-ext <gerard.fabrego.ext@adevinta.com>
2023-05-09 08:46:40 +0200

    Initialize window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => control_flow.set_exit(),
+            Event::MainEventsCleared =>
+                window.request_redraw(),
+            Event::RedrawRequested(_) => {},
+            _ => ()
+        }
+    });
 }
