67505cb81b6447ea414e58d6eccad3cee8112b45
gerard-fabrego-ext <gerard.fabrego.ext@adevinta.com>
2023-05-09 09:37:53 +0200

    Format and use clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -16,71 +16,75 @@ fn main() {
 
     let surface = unsafe { instance.create_surface(&window) }.unwrap();
 
-    let adapter = pollster::block_on(instance.request_adapter(
-        &wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            compatible_surface: Some(&surface),
-            force_fallback_adapter: false,
-        },
-    )).unwrap();
+    let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::default(),
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+    }))
+    .unwrap();
 
     let (device, queue) = pollster::block_on(adapter.request_device(
         &wgpu::DeviceDescriptor {
             features: wgpu::Features::empty(),
-            limits:
-                wgpu::Limits::default()
-            ,
+            limits: wgpu::Limits::default(),
             label: None,
         },
         None,
-    )).unwrap();
+    ))
+    .unwrap();
 
     let size = window.inner_size();
 
     let surface_caps = surface.get_capabilities(&adapter);
-    let surface_format = surface_caps.formats.iter()
+    let surface_format = surface_caps
+        .formats
+        .iter()
         .copied()
-        .filter(|f| f.is_srgb())
-        .next()
+        .find(|f| f.is_srgb())
         .unwrap_or(surface_caps.formats[0]);
 
-    surface.configure(&device, &wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: surface_format,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
-        alpha_mode: Default::default(),
-        view_formats: vec![],
-    });
+    surface.configure(
+        &device,
+        &wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: Default::default(),
+            view_formats: vec![],
+        },
+    );
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
 
         match event {
-            Event::WindowEvent {
-                ref event,
-                ..
-            } => match event {
+            Event::WindowEvent { ref event, .. } => match event {
                 WindowEvent::CloseRequested => control_flow.set_exit(),
                 WindowEvent::Resized(physical_size) => {
-                    surface.configure(&device, &wgpu::SurfaceConfiguration {
-                        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-                        format: surface_format,
-                        width: physical_size.width,
-                        height: physical_size.height,
-                        present_mode: wgpu::PresentMode::Fifo,
-                        alpha_mode: Default::default(),
-                        view_formats: vec![],
-                    });
-                },
+                    surface.configure(
+                        &device,
+                        &wgpu::SurfaceConfiguration {
+                            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                            format: surface_format,
+                            width: physical_size.width,
+                            height: physical_size.height,
+                            present_mode: wgpu::PresentMode::Fifo,
+                            alpha_mode: Default::default(),
+                            view_formats: vec![],
+                        },
+                    );
+                }
                 _ => {}
-            }
+            },
 
             Event::MainEventsCleared => window.request_redraw(),
             Event::RedrawRequested(_) => {
                 let output = surface.get_current_texture().unwrap();
-                let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+                let view = output
+                    .texture
+                    .create_view(&wgpu::TextureViewDescriptor::default());
                 let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
                     label: Some("Render Encoder"),
                 });
